35414d73e057c7d433e17953c3f69c11ffd8a4db
bosz <bobuskyyserhiy@gmail.com>
2017-02-12 21:08:17 +0000

    crazy search repository

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,23 +5,23 @@
 -- =================================================================================================
 -- AUSTRALIA
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('1','1','London', 'Brisbane', '2017-03-14 10:30:00.000', '2017-03-14 17:30:00.000', '250', 'EasyJet', '299', '29', '0', 'GBP')
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('1','1','Brisbane', 'London', '2017-03-21 10:30:00.000', '2017-03-21 17:30:00.000', '250', 'EasyJet', '299', '29', '0', 'GBP')
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('2','2','London', 'Melbourne', '2017-03-13 12:30:00.000', '2017-03-13 21:30:00.000', '250', 'Thomson Airways', '199', '29', '50', 'GBP')
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('2','2','Melbourne', 'London', '2017-03-25 12:30:00.000', '2017-03-25 21:30:00.000', '250', 'Thomson Airways', '199', '29', '50', 'GBP')
 -- Sydney
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('1','1','London', 'Melbourne', '2017-03-13 12:30:00.000', '2017-03-13 21:30:00.000', '250', 'Thomson Airways', '199', '29', '50', 'GBP')
 
-insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,airline,base_price,tax,discount)
+insert into flight(provider, cabin_class, origin, destination,departure_date,arrival_date,passengers_count,air_line,base_price,tax,discount)
  values ('1','1','Melbourne', 'London', '2017-03-25 12:30:00.000', '2017-03-25 21:30:00.000', '250', 'Thomson Airways', '199', '29', '50', 'GBP')
 
 -- =================================================================================================
